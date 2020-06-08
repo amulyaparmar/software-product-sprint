72396d245e78b502dde4fc0c0de5d8bf5c9b2140
Amulya Parmar <amulya@umich.edu>
2020-06-08 10:35:51 -0400

    add bio to portfolio and list of 2 projects

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I am a builder', 'I am blessed', 'I am grateful', 'I am a creator'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
